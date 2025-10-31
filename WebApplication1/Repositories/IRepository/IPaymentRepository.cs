445a755c9ece91db3d805d8a482ac41c36a6a035
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-10-31 12:51:04 +0530

    payment service is drafted

--- WebApplication1/Repositories/IRepository/IPaymentRepository.cs
+++ WebApplication1/Repositories/IRepository/IPaymentRepository.cs
@@ -1,8 +1,17 @@
+using WebApplication1.DTOs.ResponseDto.Common;
+using WebApplication1.Models;
+
 namespace WebApplication1.Repositories.IRepository
 {
     public interface IPaymentRepository
     {
         //CRUD operations
+        Task<IEnumerable<Payment>> GetAllAsync();
+        Task<Payment?> GetByIdAsync(int id);
+        Task AddAsync(Payment payment);
+        Task<Payment?> UpdateAsync(int id, Payment payment);
+
         //Custom Query Operations
+        Task<PaginationResultDto<Payment>> GetAllWithPaginationAsync(int pageNumber, int pageSize, int? transactionStatusId = null, string? searchKey = null);
     }
 }
