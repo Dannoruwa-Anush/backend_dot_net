9dcea80c7cbd9cbe171478321a822518fff04d7f
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2025-11-22 11:52:54 +0530

    full payment api is added

--- WebApplication1/Controllers/PaymentController.cs
+++ WebApplication1/Controllers/PaymentController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs.RequestDto.Payment;
+using WebApplication1.DTOs.ResponseDto.Common;
+using WebApplication1.Services.IService;
 
 namespace WebApplication1.Controllers
 {
@@ -6,7 +9,34 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")]
     public class PaymentController : ControllerBase
     {
-        //fully_pay
+        private readonly IPaymentService _service;
+
+        // Constructor
+        public PaymentController(IPaymentService service)
+        {
+            // Dependency injection
+            _service = service;
+        }
+
+        [HttpPost("process-full-payment")]
+        public async Task<IActionResult> ProcessFullPaymentPayment(PaymentRequestDto paymentRequest)
+        {
+            try
+            {
+                await _service.ProcessFullPaymentPaymentAsync(paymentRequest);
+                return Ok(new ApiResponseDto<string>(
+                    200,
+                    "Full payment processed and stored."
+                ));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ApiResponseDto<string>(
+                    500,
+                    "An internal server error occurred. Please try again later."
+                ));
+            }
+        }
         
         //bnpl_ installment pay
     }
