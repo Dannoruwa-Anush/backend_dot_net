a79230793379b8be9ebb484c5f59d5133d06a988
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-01-07 21:02:29 +0530

    db is updated

---
+++ WebApplication1/Migrations/20260107153057_modified_db_v2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class modified_db_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBnplPlanExist",
+                table: "CustomerOrders");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaidAt",
+                table: "Invoices",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefundedAt",
+                table: "Invoices",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VoidedAt",
+                table: "Invoices",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderPaymentMode",
+                table: "CustomerOrders",
+                type: "nvarchar(20)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Defaulted",
+                table: "BNPL_PLANs",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaidAt",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "RefundedAt",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "VoidedAt",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "OrderPaymentMode",
+                table: "CustomerOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Defaulted",
+                table: "BNPL_PLANs");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBnplPlanExist",
+                table: "CustomerOrders",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
