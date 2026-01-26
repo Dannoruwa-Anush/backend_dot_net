dfbcc9afe1c0c16a47b73b5ca07174c5fb51e597
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-01-26 21:48:16 +0530

    invoice response is modified

--- WebApplication1/DTOs/ResponseDto/InvoiceResponseDto.cs
+++ WebApplication1/DTOs/ResponseDto/InvoiceResponseDto.cs
@@ -14,5 +14,6 @@ namespace WebApplication1.DTOs.ResponseDto
         public InvoiceStatusEnum InvoiceStatus { get; set; } = InvoiceStatusEnum.Unpaid;
 
         public string? InvoiceFileUrl { get; set; }
+        public string? ReceiptFileUrl { get; set; }
     }
 }
