64a5013fcafb03c3d754695dfde65e7e5f6a423d
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-01-29 23:49:02 +0530

    comment is added

--- WebApplication1/Services/IService/Audit/IRequestContextService.cs
+++ WebApplication1/Services/IService/Audit/IRequestContextService.cs
@@ -5,6 +5,6 @@ namespace WebApplication1.Services.IService.Audit
     public interface IRequestContextService
     {
         string? IpAddress { get; }
-        string? UserAgent { get; }
+        string? UserAgent { get; } // what kind of client is making the request, including: Browser or app name, Version, Operating system, Device type
     }
 }
