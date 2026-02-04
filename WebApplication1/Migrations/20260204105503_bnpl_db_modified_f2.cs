77cd2ddf4860a43f352e48df773c176e7748306a
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-02-04 16:26:09 +0530

    order_cashflow tables are modified

---
+++ WebApplication1/Migrations/20260204105503_bnpl_db_modified_f2.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class bnpl_db_modified_f2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvoicePaymentChannel",
+                table: "Invoices",
+                type: "nvarchar(20)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PhysicalShopSessionId",
+                table: "Cashflows",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cashflows_PhysicalShopSessionId",
+                table: "Cashflows",
+                column: "PhysicalShopSessionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cashflows_PhysicalShopSessions_PhysicalShopSessionId",
+                table: "Cashflows",
+                column: "PhysicalShopSessionId",
+                principalTable: "PhysicalShopSessions",
+                principalColumn: "PhysicalShopSessionID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cashflows_PhysicalShopSessions_PhysicalShopSessionId",
+                table: "Cashflows");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cashflows_PhysicalShopSessionId",
+                table: "Cashflows");
+
+            migrationBuilder.DropColumn(
+                name: "InvoicePaymentChannel",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "PhysicalShopSessionId",
+                table: "Cashflows");
+        }
+    }
+}
